d8eef39f72053a5c40a7994081ff137671c9ff38
André Kristensen <slotkristensen@gmail.com>
2023-06-29 13:46:43 +0200

    Made visual representation of how neurons work and how to manipulate it with weights and bias

---
+++ PythonDeepLearning/NNIntro.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+weight_value = 1000
+
+bias_value_1 = 30
+
+bias_value_2 = -50
+
+plt.axis([-10, 10, -1, 10])
+
+print("The step function starts at {0} and ends at {1}".format(-bias_value_1 / weight_value, -bias_value_2 / weight_value))
+
+inputs = np.arange(-10, 10, 0.01)
+outputs = list()
+
+for x in inputs:
+    y1 = 1.0 / (1.0 + np.exp(-weight_value * x - bias_value_1))
+    y2 = 1.0 / (1.0 + np.exp(-weight_value * x - bias_value_2))
+
+    w = 2
+
+    y = y1 * w - y2 * w
+
+    outputs.append(y)
+
+plt.plot(inputs, outputs, lw=2, color ='black')
+plt.show()
